25d7a911aafdf23e7f710f9d59298615fd78cb1d
Sourov <sourovdas730@gmail.com>
2024-06-10 15:37:40 +0530

    complete category search functionality

--- src/components/MealCard.jsx
+++ src/components/MealCard.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
-const MealCard = ({meal}) => {
-    const {image, title, price, category} = meal;
+const MealCard = ({ meal }) => {
+    const { image, title, price, category } = meal;
     return (
-        <div className='h-[300px] bg-cover bg-center font-roboto' style={{backgroundImage : `url(${image})`}}>
-            <div className='h-full w-full flex flex-col justify-between bg-black bg-opacity-10'>
+        <div className='h-[300px] bg-cover bg-center font-roboto' style={{ backgroundImage: `url(${image})` }}>
+            <div className='h-full w-full flex flex-col justify-between bg-black bg-opacity-10 hover:bg-opacity-30'>
                 <h2 className='w-full flex justify-end py-3 px-2 opacity-100 text-2xl'><span className='bg-black bg-opacity-30 text-white opacity-90 px-1 py-1'>${price}</span></h2>
                 <div className='bg-black bg-opacity-40 py-3 px-2 flex justify-between items-center gap-6'>
-                    <h2 className='text-xl font-bold text-white opacity-90'>{title}</h2>
+                    <div className='flex flex-col justify-center text-sm text-white opacity-90'>
+                        <h2 className='text-xl font-bold text-white opacity-90'>{title}</h2>
+                        <p>For <span className='underline'>{category}</span></p>
+                    </div>
                     <button className='bg-white bg-opacity-50 hover:bg-opacity-60 py-1 px-2 font-medium'>Details</button>
                 </div>
             </div>
--- src/pages/meals/Meals.jsx
+++ src/pages/meals/Meals.jsx
@@ -1,41 +1,96 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosPublic from "../../hooks/useAxiosPublic";
-import { IoSearchOutline } from "react-icons/io5";
+import { IoFilterOutline, IoSearch, IoSearchOutline } from "react-icons/io5";
 import MealCard from "../../components/MealCard";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 const Meals = () => {
     const axiosPublic = useAxiosPublic();
     const [searchedMeal, setSearchedMeal] = useState([]);
+    const [searchedMeal1, setSearchedMeal1] = useState([]);
     const [searchValue, setSearchValue] = useState(null);
-    const {data : meals = []} = useQuery({
-        queryKey : ['meal'],
-        queryFn : async () => {
+    const [filter, setFilter] = useState(false);
+    const [categoryFilter, setCategoryFilter] = useState([]);
+    console.log('svalue - ', searchedMeal1)
+    const { data: meals = [] } = useQuery({
+        queryKey: ['meal'],
+        queryFn: async () => {
             const res = await axiosPublic.get('/all-category-meals')
             return res.data;
         },
     })
-    const handleSearch = async (event) =>{
+    useEffect(() => {
+        if(meals){
+            setSearchedMeal(meals);
+        }
+    }, [meals])
+
+    // setSearchedMeal(...meals);
+    const handleSearch = async (event) => {
         const searchQuery = event.target.value;
         setSearchValue(searchQuery);
         console.log(searchQuery);
         const res = await axiosPublic.get(`/search-meals/${searchQuery}`)
         console.log(res.data);
         setSearchedMeal(res.data);
+        setSearchedMeal1(res.data);
+
     }
-    console.log(meals);
-    return(
-        <div className="max-w-5xl mx-auto py-24">
+    // console.log(meals);
+    const handleCategoryFilter = async (event) => {
+        const filterValue = event.target.value;
+        console.log(filterValue);
+        if (!searchValue) {
+            const res = await axiosPublic.get(`/filter-by-category/${filterValue}`)
+            console.log(res.data);
+            setSearchedMeal(res.data);
+        }
+        if(searchValue){
+           const filterSearchValue = searchedMeal1.filter(value => filterValue === value.category);
+           console.log('ddd', filterSearchValue)
+           setSearchedMeal(filterSearchValue);
+        }
+
+    }
+    return (
+        <div className="max-w-5xl mx-auto py-24 font-roboto">
             <h1 className="text-center text-2xl font-bold mb-9">Meals</h1>
-            <div className="mb-9">
-                <input onChange={handleSearch} className="w-2/3 py-3 px-3 bg-gray-200" type="text" />
-                <button className="py-3 px-2 text-white bg-[#151515] bg-opacity-90 hover:bg-opacity-100">Search</button>
+            <div className="mb-9 flex justify-center items-center relative w-2/3 mx-auto">
+                <input onChange={handleSearch} className="w-full py-3 px-3 bg-gray-200 rounded-full" type="text" placeholder="pizza" />
+                <span className="text-xl absolute top right-4 opacity-80"><IoSearch /></span>
             </div>
+            <div className="mb-6 flex justify-end">
+                <button onClick={() => setFilter(!filter)} className="border hover:border-black hover:border-opacity-70 py-3 px-4 flex justify-center items-center gap-2">
+                    <span><IoFilterOutline /></span>
+                    <span>Filter</span>
+                </button>
+            </div>
+            {
+                filter &&
+                <div className="flex justify-end items-center gap-9">
+                    <div className="mb-6 flex justify-end">
+                        <select onChange={handleCategoryFilter} className="bg-gray-200 py-3 px-2 border-2 border-opacity-80 border-black" name="" id="">
+                            <option value="category">Category</option>
+                            <option value="breakfast">Breakfast</option>
+                            <option value="lunch">Lunch</option>
+                            <option value="dinner">Dinner</option>
+                        </select>
+                    </div>
+                    <div className="mb-6 flex justify-end">
+                        <select className="bg-gray-200 py-3 px-2 border-2 border-opacity-80 border-black" name="" id="">
+                            <option value="filter">Price($)</option>
+                            <option value="category">0 - 20</option>
+                            <option value="">20 - 40</option>
+                            <option value="">40 up</option>
+                        </select>
+                    </div>
+                </div>
+            }
             <div className="max-w-5xl mx-auto grid grid-cols-3 gap-6">
                 {
-                  !searchValue ? meals.map(meal => <MealCard meal={meal} key={meal._id}></MealCard>):
-                  searchedMeal.map(meal => <MealCard meal={meal} key={meal._id}></MealCard>)
+                    !searchedMeal?.length > 0 ? <p className="text text-4xl">Not found</p> :
+                        searchedMeal.map(meal => <MealCard meal={meal} key={meal._id}></MealCard>)
                 }
             </div>
         </div>
