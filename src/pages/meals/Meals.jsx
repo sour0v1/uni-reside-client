8ea16546d014a0d8b667e825684c0a7be41b3bdc
Sourov <sourovdas730@gmail.com>
2024-06-18 21:43:12 +0600

    complete meal search functionality

--- src/pages/meals/Meals.jsx
+++ src/pages/meals/Meals.jsx
@@ -16,6 +16,7 @@ const Meals = () => {
     const [page, setPage] = useState(1)
     const [hasMore, setHasMore] = useState(true)
     console.log('svalue - ', meals)
+    console.log('svalue1 - ', meals1)
 
     const loadMeals = async () => {
         const res = await axiosPublic.get(`/all-category-meals?page=${page}&limit=${10}`)
@@ -33,8 +34,8 @@ const Meals = () => {
         setSearchValue(searchQuery);
         console.log(searchQuery);
         const res = await axiosPublic.get(`/search-meals/${searchQuery}`)
-        console.log(res.data);
-        setMeals(res.data);
+        console.log('err',res.data);
+        // setMeals(res.data);
         setMeals1(res.data);
 
     }
@@ -59,30 +60,19 @@ const Meals = () => {
             dataLength={meals?.length} //This is important field to render the next data
             next={loadMeals}
             hasMore={hasMore}
-            loader={<h4>Loading...</h4>}
-            endMessage={
-                <p style={{ textAlign: 'center' }}>
-                    <b>Yay! You have seen it all</b>
-                </p>
-            }
+            loader={<h4 className="text-center">Loading...</h4>}
         >
-            <div className="max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 font-roboto">
-                {
-                    meals?.map((meal, idx) => <div key={idx} className=" bg-base-100 shadow-xl">
-                        <figure><img className="h-[200px]" src={meal?.image} alt="Shoes" /></figure>
-                        <div className="card-body">
-                            <div className="flex justify-between items-center">
-                                <h2 className="card-title">{meal?.title}</h2>
-                                <h2 className="card-title">${meal?.price}</h2>
-                            </div>
-                            <p>Category : {meal?.category}</p>
-                            <p>Rating : {meal?.rating}</p>
-                            <div className="card-actions justify-end">
-                                <Link to={`/meal/${meal?._id}`} className="btn">View</Link >
-                            </div>
-                        </div>
-                    </div>)
-                }
+            <div className="max-w-5xl mx-auto font-roboto pt-24">
+                <h1 className="w-full text-center text-2xl font-bold">All Meals</h1>
+                <div className="w-2/3 mx-auto my-6 text-center relative">
+                    <input onChange={handleSearch} placeholder="meal name" className="py-2 px-3 w-full bg-gray-200 rounded-full outline-none" type="text" />
+                    <span className="absolute text-xl right-3 top-2 opacity-90 "><IoSearchOutline /></span>
+                </div>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                    {
+                        (searchValue?.length ? meals1 : meals)?.map((meal, idx) => <MealCard key={idx} meal={meal}></MealCard>)
+                    }
+                </div>
             </div>
 
         </InfiniteScroll>
